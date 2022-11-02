63530a6173f56becfd6f68d2699a327afb098d90
Hungho9286 <0306201537@caothang.edu.vn>
2022-11-02 10:02:34 +0700

    2/11/2022

--- lib/result_answer.dart
+++ lib/result_answer.dart
@@ -38,202 +38,204 @@ class _AnswerResultState extends State<AnswerResult>
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height,
-      color: Color.fromARGB(255, 242, 253, 255),
-      padding: EdgeInsets.only(top: 20),
-      child: Stack(
-        children: [
-          AnimatedPositioned(
-            top: 0,
-            left: 0,
-            child: ScaleTransition(
-              scale: animation,
-              child: SizedBox(
-                width: 100,
-                height: 100,
-                child: Image.asset('images/curves/vector1.png'),
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        color: Color.fromARGB(255, 242, 253, 255),
+        padding: EdgeInsets.only(top: 20),
+        child: Stack(
+          children: [
+            AnimatedPositioned(
+              top: 0,
+              left: 0,
+              child: ScaleTransition(
+                scale: animation,
+                child: SizedBox(
+                  width: 100,
+                  height: 100,
+                  child: Image.asset('images/curves/vector1.png'),
+                ),
               ),
+              duration: Duration(seconds: 2),
             ),
-            duration: Duration(seconds: 2),
-          ),
-          AnimatedPositioned(
-            top: 40,
-            right: 70,
-            child: ScaleTransition(
-              scale: animation,
-              child: SizedBox(
-                width: 100,
-                height: 100,
-                child: Image.asset('images/curves/vector2.png'),
+            AnimatedPositioned(
+              top: 40,
+              right: 70,
+              child: ScaleTransition(
+                scale: animation,
+                child: SizedBox(
+                  width: 100,
+                  height: 100,
+                  child: Image.asset('images/curves/vector2.png'),
+                ),
               ),
+              duration: Duration(seconds: 2),
             ),
-            duration: Duration(seconds: 2),
-          ),
-          AnimatedPositioned(
-            bottom: 100,
-            right: 12,
-            child: ScaleTransition(
-              scale: animation,
-              child: SizedBox(
-                width: 100,
-                height: 100,
-                child: Image.asset('images/curves/vector3.png'),
+            AnimatedPositioned(
+              bottom: 100,
+              right: 12,
+              child: ScaleTransition(
+                scale: animation,
+                child: SizedBox(
+                  width: 100,
+                  height: 100,
+                  child: Image.asset('images/curves/vector3.png'),
+                ),
               ),
+              duration: Duration(seconds: 2),
             ),
-            duration: Duration(seconds: 2),
-          ),
-          AnimatedPositioned(
-            top: 120,
-            left: 149,
-            child: ScaleTransition(
-              scale: animation,
-              child: SizedBox(
-                width: 100,
-                height: 100,
-                child: Image.asset('images/curves/vector2.png'),
+            AnimatedPositioned(
+              top: 120,
+              left: 149,
+              child: ScaleTransition(
+                scale: animation,
+                child: SizedBox(
+                  width: 100,
+                  height: 100,
+                  child: Image.asset('images/curves/vector2.png'),
+                ),
               ),
+              duration: Duration(seconds: 2),
             ),
-            duration: Duration(seconds: 2),
-          ),
-          AnimatedPositioned(
-            bottom: 137,
-            left: 12,
-            child: ScaleTransition(
-              scale: animation,
-              child: SizedBox(
-                width: 100,
-                height: 100,
-                child: Image.asset('images/curves/vector7.png'),
+            AnimatedPositioned(
+              bottom: 137,
+              left: 12,
+              child: ScaleTransition(
+                scale: animation,
+                child: SizedBox(
+                  width: 100,
+                  height: 100,
+                  child: Image.asset('images/curves/vector7.png'),
+                ),
               ),
+              duration: Duration(seconds: 2),
             ),
-            duration: Duration(seconds: 2),
-          ),
-          AnimatedPositioned(
-            top: 149,
-            left: 12,
-            child: ScaleTransition(
-              scale: animation,
-              child: SizedBox(
-                width: 100,
-                height: 100,
-                child: Image.asset('images/curves/vector8.png'),
+            AnimatedPositioned(
+              top: 149,
+              left: 12,
+              child: ScaleTransition(
+                scale: animation,
+                child: SizedBox(
+                  width: 100,
+                  height: 100,
+                  child: Image.asset('images/curves/vector8.png'),
+                ),
               ),
+              duration: Duration(seconds: 2),
             ),
-            duration: Duration(seconds: 2),
-          ),
-          AnimatedPositioned(
-            top: 240,
-            right: 156,
-            child: ScaleTransition(
-              scale: animation,
-              child: SizedBox(
-                width: 100,
-                height: 100,
-                child: Image.asset('images/curves/vector3.png'),
+            AnimatedPositioned(
+              top: 240,
+              right: 156,
+              child: ScaleTransition(
+                scale: animation,
+                child: SizedBox(
+                  width: 100,
+                  height: 100,
+                  child: Image.asset('images/curves/vector3.png'),
+                ),
               ),
+              duration: Duration(seconds: 2),
             ),
-            duration: Duration(seconds: 2),
-          ),
-          AnimatedPositioned(
-            bottom: 0,
-            right: 178,
-            child: ScaleTransition(
-              scale: animation,
-              child: SizedBox(
-                width: 100,
-                height: 100,
-                child: Image.asset('images/curves/vector5.png'),
+            AnimatedPositioned(
+              bottom: 0,
+              right: 178,
+              child: ScaleTransition(
+                scale: animation,
+                child: SizedBox(
+                  width: 100,
+                  height: 100,
+                  child: Image.asset('images/curves/vector5.png'),
+                ),
               ),
+              duration: Duration(seconds: 2),
             ),
-            duration: Duration(seconds: 2),
-          ),
-          AnimatedPositioned(
-            bottom: 236,
-            left: 353,
-            child: ScaleTransition(
-              scale: animation,
-              child: SizedBox(
-                width: 100,
-                height: 100,
-                child: Image.asset('images/curves/vector3.png'),
+            AnimatedPositioned(
+              bottom: 236,
+              left: 353,
+              child: ScaleTransition(
+                scale: animation,
+                child: SizedBox(
+                  width: 100,
+                  height: 100,
+                  child: Image.asset('images/curves/vector3.png'),
+                ),
               ),
+              duration: Duration(seconds: 2),
             ),
-            duration: Duration(seconds: 2),
-          ),
-          AnimatedPositioned(
-            top: 325,
-            left: 179,
-            child: ScaleTransition(
-              scale: animation,
-              child: SizedBox(
-                width: 100,
-                height: 100,
-                child: Image.asset('images/curves/vector8.png'),
+            AnimatedPositioned(
+              top: 325,
+              left: 179,
+              child: ScaleTransition(
+                scale: animation,
+                child: SizedBox(
+                  width: 100,
+                  height: 100,
+                  child: Image.asset('images/curves/vector8.png'),
+                ),
               ),
+              duration: Duration(seconds: 2),
             ),
-            duration: Duration(seconds: 2),
-          ),
-          AnimatedPositioned(
-            child: ScaleTransition(
-              scale: animation,
-              child: SizedBox(
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  alignment: Alignment.center,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset('images/curves/girl.png'),
-                      SizedBox(
-                        height: 45,
-                        width: 20,
-                      ),
-                      Text(
-                        'CONGRATS',
-                        style: TextStyle(
-                          color: Color.fromARGB(199, 30, 30, 30),
-                          fontWeight: FontWeight.bold,
-                          fontSize: 45,
+            AnimatedPositioned(
+              child: ScaleTransition(
+                scale: animation,
+                child: SizedBox(
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    alignment: Alignment.center,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset('images/curves/girl.png'),
+                        SizedBox(
+                          height: 45,
+                          width: 20,
                         ),
-                      ),
-                      ElevatedButton(
-                        onPressed: () => Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => ChooseCategories(),
-                          ),
-                        ),
-                        style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all(
-                            Color.fromARGB(255, 134, 241, 211),
-                          ),
-                        ),
-                        child: Text(
-                          'Tiếp tục',
+                        Text(
+                          'CONGRATS',
                           style: TextStyle(
                             color: Color.fromARGB(199, 30, 30, 30),
                             fontWeight: FontWeight.bold,
+                            fontSize: 45,
                           ),
                         ),
-                      ),
-                      SizedBox(
-                        height: 175,
-                        width: 20,
-                      ),
-                      SizedBox(
-                        height: 45,
-                        width: 20,
-                      ),
-                    ],
+                        ElevatedButton(
+                          onPressed: () => Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ChooseCategories(),
+                            ),
+                          ),
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all(
+                              Color.fromARGB(255, 134, 241, 211),
+                            ),
+                          ),
+                          child: Text(
+                            'Tiếp tục',
+                            style: TextStyle(
+                              color: Color.fromARGB(199, 30, 30, 30),
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 175,
+                          width: 20,
+                        ),
+                        SizedBox(
+                          height: 45,
+                          width: 20,
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
+              duration: Duration(seconds: 2),
             ),
-            duration: Duration(seconds: 2),
-          ),
-        ],
+          ],
+        ),
       ),
     );
     ;
